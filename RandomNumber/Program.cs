6c03d751004818ae5e6c0c1467566796452847b4
Vilmar Viimsalu <vilmar.viimsalu@tktk.ee>
2024-08-17 14:11:54 +0300

    week 4

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11); //genereeritakse väärtusi 0..10-ni
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is {myRandomNum}");
+
+}
+
+Console.WriteLine($"Random sum total; {randomSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//FOR-LOOP
+//DRY-principle -Do Not Repeat Yourself
+
+ //  (START;  END; STEP)
+for (int i = 0; i<5; i++) //i++ = i+1
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day");
